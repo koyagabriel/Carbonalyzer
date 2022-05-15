89669594c7433d48c67e95100e4ede6472dc5019
koya <koyexes@gmail.com>
2022-05-15 14:17:04 +0100

    Register routes for user, usage and usage_type resources.

---
+++ server/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from api.views.token import TokenView
+from api.views.users import (UsersCreateView, UserProfileView, UserProfileUpdateView)
+from api.views.usage_types import (UsageTypesListView, UsageTypeRetrieveView)
+from api.views.usages import (UsageListCreateView, UsageRetrieveUpdateView)
+
+urlpatterns = [
+    path("register", UsersCreateView.as_view(), name="registration"),
+    path("token", TokenView.as_view(), name="token"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("profile", UserProfileView.as_view(), name="user_profile"),
+    path("profile/update", UserProfileUpdateView.as_view(), name="user_profile_update"),
+    path("usage-types", UsageTypesListView.as_view(), name="usage_types"),
+    path("usage-types/<int:id>", UsageTypeRetrieveView.as_view(), name="retrieve_usage_types"),
+    path("usages", UsageListCreateView.as_view(), name="usages"),
+    path("usages/<int:id>", UsageRetrieveUpdateView.as_view(), name="retrieve_update_usages")
+]
